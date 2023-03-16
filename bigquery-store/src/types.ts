973ba2d2c53f67a9ec7c1556d187aa3be5b7b53b
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-03-16 19:20:19 +0500

    add precision and scale parametrs for Numeric and BigNumeric types

--- bigquery-store/src/types.ts
+++ bigquery-store/src/types.ts
@@ -1,4 +1,5 @@
-import {BigQueryInt, BigQueryTimestamp} from "@google-cloud/bigquery"
+import {BigQueryInt, BigQueryTimestamp} from '@google-cloud/bigquery'
+import assert from 'assert'
 
 export interface Type<T> {
     bqType: string
@@ -14,18 +15,22 @@ export function StringType(): Type<string> {
     }
 }
 
-export function NumericType(): Type<bigint> {
+export function NumericType(precision: number, scale = 0): Type<bigint> {
+    assert(Number.isSafeInteger(precision) && precision > 0 && precision <= 38, 'Invalid precision')
+    assert(Number.isSafeInteger(scale) && scale > 0 && scale <= precision, 'Invalid scale')
     return {
-        bqType: 'NUMERIC',
+        bqType: `NUMERIC(${precision}, ${scale})`,
         serialize(value) {
             return value.toString()
         },
     }
 }
 
-export function BigNumericType(): Type<bigint> {
+export function BigNumericType(precision: number, scale = 0): Type<bigint> {
+    assert(Number.isSafeInteger(precision) && precision > 0 && precision <= 76, 'Invalid precision')
+    assert(Number.isSafeInteger(scale) && scale > 0 && scale <= precision, 'Invalid scale')
     return {
-        bqType: 'BIGNUMERIC',
+        bqType: `BIGNUMERIC(${precision}, ${scale})`,
         serialize(value) {
             return value.toString()
         },

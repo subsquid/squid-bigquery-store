3ac3803fdad78eef5bc81e42a6ff0dc337e89281
abernatskiy <abernatskiy@toha.li>
2023-11-13 17:22:04 +0900

    Updating the test to ArrowSquid
    	modified:   common/config/rush/pnpm-lock.yaml
    	modified:   test/Makefile
    	modified:   test/package.json
    	new file:   test/src/main.ts
    	modified:   test/src/processor.ts
    	modified:   test/src/tables.ts
    	new file:   test/src/types/balances/events.ts
    	modified:   test/src/types/events.ts
    	new file:   test/src/types/index.ts
    	modified:   test/src/types/support.ts
    	new file:   test/src/types/v1020.ts
    	new file:   test/src/types/v1050.ts
    	new file:   test/src/types/v9130.ts
    	modified:   test/typegen.json

--- test/src/processor.ts
+++ test/src/processor.ts
@@ -1,98 +1,25 @@
-import * as ss58 from '@subsquid/ss58'
-import {decodeHex, SubstrateBatchProcessor, SubstrateBlock} from '@subsquid/substrate-processor'
-import {Database} from '@subsquid/bigquery-store'
-import {BalancesTransferEvent} from './types/events'
-import {Extrinsics, Transfers} from './tables'
-import {BigQuery} from '@google-cloud/bigquery'
+import {SubstrateBatchProcessor} from '@subsquid/substrate-processor'
+import {events} from './types'
 
-const processor = new SubstrateBatchProcessor()
+export const processor = new SubstrateBatchProcessor()
     .setDataSource({
-        archive: 'https://kusama.archive.subsquid.io/graphql',
+        archive: 'https://v2.archive.subsquid.io/network/kusama',
+        chain: {
+            url: 'https://kusama-rpc.polkadot.io',
+            rateLimit: 10
+        }
     })
-    .addEvent('Balances.Transfer', {
-        data: {
-            event: {
-                args: true,
-                extrinsic: {
-                    hash: true,
-                    call: {
-                        origin: true,
-                    },
-                },
-            },
+    .addEvent({
+        name: [events.balances.transfer.name],
+        extrinsic: true,
+        call: true
+    })
+    .setFields({
+        block: {
+            timestamp: true
         },
-    } as const)
-
-let bq = new BigQuery({
-    projectId: `bright-meridian-316511`,
-    credentials: {
-        // your cridentials
-    },
-})
-
-let db = new Database({
-    tables: {
-        Transfers,
-        Extrinsics,
-    },
-    bq,
-    dataset: 'bigquery_store'
-})
-
-processor.run(db, async (ctx) => {
-    for (let block of ctx.blocks) {
-        let prevExtrinsic: string | undefined
-        for (let item of block.items) {
-            if (item.name == 'Balances.Transfer') {
-                let e = new BalancesTransferEvent(ctx, item.event)
-                let rec: {from: Uint8Array; to: Uint8Array; amount: bigint}
-                if (e.isV1020) {
-                    let [from, to, amount] = e.asV1020
-                    rec = {from, to, amount}
-                } else if (e.isV1050) {
-                    let [from, to, amount] = e.asV1050
-                    rec = {from, to, amount}
-                } else if (e.isV9130) {
-                    rec = e.asV9130
-                } else {
-                    throw new Error('Unsupported spec')
-                }
-
-                ctx.store.Transfers.insert({
-                    blockNumber: block.header.height,
-                    timestamp: new Date(block.header.timestamp),
-                    extrinsicHash: item.event.extrinsic?.hash,
-                    from: ss58.codec('kusama').encode(rec.from),
-                    to: ss58.codec('kusama').encode(rec.to),
-                    amount: rec.amount,
-                })
-
-                if (item.event.extrinsic && prevExtrinsic != item.event.extrinsic.hash) {
-                    let signer = getOriginAccountId(item.event.extrinsic.call.origin)
-
-                    if (signer) {
-                        ctx.store.Extrinsics.insert({
-                            blockNumber: block.header.height,
-                            timestamp: new Date(block.header.timestamp),
-                            hash: item.event.extrinsic.hash,
-                            signer: ss58.codec('kusama').encode(signer),
-                        })
-                    }
-                }
-            }
+        extrinsic: {
+            hash: true
         }
-    }
-})
-
-export function getOriginAccountId(origin: any) {
-    if (origin && origin.__kind === 'system' && origin.value.__kind === 'Signed') {
-        const id = origin.value.value
-        if (id.__kind === 'Id') {
-            return decodeHex(id.value)
-        } else {
-            return decodeHex(id)
-        }
-    } else {
-        return undefined
-    }
-}
+    })
+    .setBlockRange({ from: 1000000 })
--- test/src/tables.ts
+++ test/src/tables.ts
@@ -6,12 +6,5 @@ export const Transfers = new Table('transfers', {
     extrinsicHash: Column(Types.String(), {nullable: true}),
     from: Column(Types.String()),
     to: Column(Types.String()),
-    amount: Column(Types.Int64()),
-})
-
-export const Extrinsics = new Table('extrinsics', {
-    blockNumber: Column(Types.Int64()),
-    timestamp: Column(Types.Timestamp()),
-    hash: Column(Types.String()),
-    signer: Column(Types.String()),
+    amount: Column(Types.BigNumeric(38)),
 })
